6ce7831106f0c3625a5bc1d920103ae7764b3de1
VOLP1 <eduvolpi5@gmail.com>
2026-01-21 16:29:42 -0300

    header atualizado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -29,7 +29,13 @@ async function bootstrap() {
     origin: corsOrigins,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'X-Company-Id',
+      'x-company-id',
+    ],
   });
 
   // Swagger configuration
